0060d8c32ebbdcf8e49d3f8f8f16d39437f28faa
Wataru Kurashima <watarukura@users.noreply.github.com>
2021-12-18 20:53:53 +0900

    Update review_please.js

--- review_please.js
+++ review_please.js
@@ -3,12 +3,7 @@ javascript:(function () {
         alert("failed");
         return;
     }
-    document.getElementById("new_comment_field").value = document
+    document.getElementById("new_comment_field").value = Array.from(document
 		.querySelectorAll("#reviewers-select-menu")[0]
-		.parentElement.children[3].innerText
-		.split("\n\n")
-		.map(a=>"@".concat(a))
-		.sort()
-	        .concat(["レビューお願いします:bow:"])
-		.join(" ");
+		.parentElement.children[3].querySelectorAll(".assignee")).map((elem)=> `@${elem.innerText}`).sort().concat(["レビューお願いします:bow:"]).join(" ")
 })()
